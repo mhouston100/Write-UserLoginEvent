38956638f9d4bf73c7907c5d30124dc9d43dd11a
Matthew Houston <Matthew.Houston@camden.nsw.gov.au>
2019-05-08 14:00:50 +1000

    dsfsaf

---
+++ Test.ps1
@@ -0,0 +1,11 @@
+$message = ": User initiated logoff:
+
+                       Subject:
+                        Security ID:            S-1-5-21-1343377312-1359496742-1235820382-12204
+                        Account Name:           adminfcamilleri
+                        Account Domain:         CMD_DOMAIN
+                        Logon ID:               0x319E2F43
+
+                       This event is generated when a logoff is initiated. No further user-initiated activity can occur. This event can be interpreted as a logoff event."
+
+$message
--- Write-UserLoginEvent.ps1
+++ Write-UserLoginEvent.ps1
@@ -1,10 +1,17 @@
-$allEvents = Get-WinEvent -LogName ForwardedEvents | ?{$_.ID -ne 111}
+$allEvents = Get-WinEvent -LogName ForwardedEvents | Select *
 
 $outEvents = @()
 
 foreach ($curEvent in $allEvents){
     switch ($curEvent.ID) {
-        4647 { $curEvent }
-        4624 { $curEvent }
+        4647    { $curObj = New-Object -TypeName PSObject
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name TimeCreated -Value $curEvent.TimeCreated
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name Action -Value $curEvent.TaskDisplayName
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name MachineName -Value $curEvent.MachineName 
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name UserName -Value ($curEvent.Message.Split([Environment]::NewLine)[8])
+                }
     }
+    $outEvents += $curEvent
 }
+
+$outEvents
