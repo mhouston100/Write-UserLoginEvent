12f2b54cfb9586b89d782e70bf919245830b8bdc
Matthew Houston <Matthew.Houston@camden.nsw.gov.au>
2019-05-09 10:46:31 +1000

    Added IP address

--- Write-UserLoginEvent.ps1
+++ Write-UserLoginEvent.ps1
@@ -5,22 +5,24 @@ $outEvents = @()
 foreach ($curEvent in $allEvents){
     $curObj = $null
     switch ($curEvent.ID) {
-        4647    { $curObj = New-Object -TypeName PSObject
-                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name TimeCreated -Value ([datetime]$curEvent.TimeCreated)
-                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name Action -Value $curEvent.TaskDisplayName
-                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name MachineName -Value $curEvent.MachineName 
-                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name UserName -Value ((($curEvent.Message).Split([Environment]::NewLine)[8]).split(":")[1]).Trim()
-                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name LoginID -Value ((($curEvent.Message).Split([Environment]::NewLine)[12]).split(":")[1]).Trim()
-                    #Add-Member -InputObject $curObj -MemberType NoteProperty -Name RawMessage -Value $curEvent.Message
-                }
         4624    { $curObj = New-Object -TypeName PSObject
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name TimeCreated -Value ([datetime]$curEvent.TimeCreated)
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name Action -Value $curEvent.TaskDisplayName
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name MachineName -Value $curEvent.MachineName 
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name UserName -Value ((($curEvent.Message).Split([Environment]::NewLine)[36]).split(":")[1]).Trim()
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name LoginID -Value ((($curEvent.Message).Split([Environment]::NewLine)[40]).split(":")[1]).Trim()
-                    #Add-Member -InputObject $curObj -MemberType NoteProperty -Name RawMessage -Value $curEvent.Message
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name SourceIP -Value ((($curEvent.Message).Split([Environment]::NewLine)[64]).split(":")[1]).Trim()
                 }
+        4647    { $curObj = New-Object -TypeName PSObject
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name TimeCreated -Value ([datetime]$curEvent.TimeCreated)
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name Action -Value $curEvent.TaskDisplayName
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name MachineName -Value $curEvent.MachineName 
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name UserName -Value ((($curEvent.Message).Split([Environment]::NewLine)[8]).split(":")[1]).Trim()
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name LoginID -Value ((($curEvent.Message).Split([Environment]::NewLine)[12]).split(":")[1]).Trim()
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name SourceIP -Value "0.0.0.0"
+
+                }
+
         Default { }
     }
     $outEvents += $curObj
