3b0ebd34e58980f54a1929ec97fa55c46ec7bbdd
Matthew Houston <Matthew.Houston@camden.nsw.gov.au>
2019-05-10 10:47:01 +1000

    Return Data Table

--- Write-UserLoginEvent.ps1
+++ Write-UserLoginEvent.ps1
@@ -1,6 +1,8 @@
+Import-Module .\DataTable.psm1
+
 $allEvents = Get-WinEvent -LogName ForwardedEvents | ?{$_.Id -ne 111}
 
-$outEvents = @()
+$outEvents = New-Object -TypeName System.Collections.ArrayList
 
 foreach ($curEvent in $allEvents){
     $curObj = $null
@@ -12,7 +14,8 @@ foreach ($curEvent in $allEvents){
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name UserName -Value ((($curEvent.Message).Split([Environment]::NewLine)[36]).split(":")[1]).Trim()
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name LoginID -Value ((($curEvent.Message).Split([Environment]::NewLine)[40]).split(":")[1]).Trim()
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name SourceIP -Value ((($curEvent.Message).Split([Environment]::NewLine)[64]).split(":")[1]).Trim()
-                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name EventID -Value $curEvent.Id
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name ID -Value $curEvent.Id
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name RecordID -Value $curEvent.EventRecordID
                 }
         4647    { $curObj = New-Object -TypeName PSObject
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name TimeCreated -Value ([datetime]$curEvent.TimeCreated)
@@ -21,7 +24,8 @@ foreach ($curEvent in $allEvents){
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name UserName -Value ((($curEvent.Message).Split([Environment]::NewLine)[8]).split(":")[1]).Trim()
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name LoginID -Value ((($curEvent.Message).Split([Environment]::NewLine)[12]).split(":")[1]).Trim()
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name SourceIP -Value ""
-                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name EventID -Value $curEvent.Id
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name ID -Value $curEvent.Id
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name RecordID -Value $curEvent.EventRecordID
                 }
         4778    { $curObj = New-Object -TypeName PSObject
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name TimeCreated -Value ([datetime]$curEvent.TimeCreated)
@@ -30,7 +34,8 @@ foreach ($curEvent in $allEvents){
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name UserName -Value ((($curEvent.Message).Split([Environment]::NewLine)[6]).split(":")[1]).Trim()
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name LoginID -Value ((($curEvent.Message).Split([Environment]::NewLine)[10]).split(":")[1]).Trim()
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name SourceIP -Value ((($curEvent.Message).Split([Environment]::NewLine)[24]).split(":")[1]).Trim()
-                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name EventID -Value $curEvent.Id
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name ID -Value $curEvent.Id
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name RecordID -Value $curEvent.EventRecordID
                 }
         4800    { $curObj = New-Object -TypeName PSObject
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name TimeCreated -Value ([datetime]$curEvent.TimeCreated)
@@ -39,7 +44,8 @@ foreach ($curEvent in $allEvents){
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name UserName -Value ((($curEvent.Message).Split([Environment]::NewLine)[8]).split(":")[1]).Trim()
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name LoginID -Value ((($curEvent.Message).Split([Environment]::NewLine)[12]).split(":")[1]).Trim()
                     Add-Member -InputObject $curObj -MemberType NoteProperty -Name SourceIP -Value ""
-                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name EventID -Value $curEvent.Id
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name ID -Value $curEvent.Id
+                    Add-Member -InputObject $curObj -MemberType NoteProperty -Name RecordID -Value $curEvent.EventRecordID
                 }
 
         Default { }
@@ -47,4 +53,5 @@ foreach ($curEvent in $allEvents){
     $outEvents += $curObj
 }
 
-$outEvents | Sort-Object TimeCreated
+$dataTable = Out-DataTable $outEvents
+
