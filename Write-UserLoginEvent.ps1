29ccd49a5b293880187f206c724f7140bf7cc9f2
Matthew Houston <Matthew.Houston@camden.nsw.gov.au>
2019-05-20 13:27:52 +1000

    Merged datatable back into Module

--- Write-UserLoginEvent.ps1
+++ Write-UserLoginEvent.ps1
@@ -1,81 +1,8 @@
-function Get-Type 
-{ 
-    param($type) 
- 
-$types = @( 
-'System.Boolean', 
-'System.Byte[]', 
-'System.Byte', 
-'System.Char', 
-'System.Datetime', 
-'System.Decimal', 
-'System.Double', 
-'System.Guid', 
-'System.Int16', 
-'System.Int32', 
-'System.Int64', 
-'System.Single', 
-'System.UInt16', 
-'System.UInt32', 
-'System.UInt64') 
- 
-    if ( $types -contains $type ) { 
-        Write-Output "$type" 
-    } 
-    else { 
-        Write-Output 'System.String' 
-         
-    } 
-} #Get-Type 
- 
-function Out-DataTable
-{ 
-    [CmdletBinding()] 
-    param([Parameter(Position=0, Mandatory=$true, ValueFromPipeline = $true)] [PSObject[]]$InputObject) 
- 
-    Begin 
-    { 
-        $dt = new-object Data.datatable   
-        $First = $true  
-    } 
-    Process 
-    { 
-        Write-Output "test"
-        foreach ($object in $InputObject) 
-        { 
-            $DR = $DT.NewRow()
-            foreach($property in $object.PsObject.get_properties()) 
-            {   
-                if ($first) 
-                {   
-                    $Col =  new-object Data.DataColumn   
-                    $Col.ColumnName = $property.Name.ToString()   
-                    if ($property.value) 
-                    { 
-                        if ($property.value -isnot [System.DBNull]) { 
-                            $Col.DataType = [System.Type]::GetType("$(Get-Type $property.TypeNameOfValue)") 
-                         } 
-                    } 
-                    $DT.Columns.Add($Col) 
-                }   
-                if ($property.Gettype().IsArray) { 
-                    $DR.Item($property.Name) =$property.value | ConvertTo-XML -AS String -NoTypeInformation -Depth 1 
-                }   
-               else { 
-                    $DR.Item($property.Name) = $property.value 
-                } 
-            }   
-            $DT.Rows.Add($DR)   
-            $First = $false 
-        } 
-    }  
-      
-    End 
-    { 
-        Write-Output @(,($dt)) 
-    } 
- 
-} #Out-DataTable
+Import-Module .\DataTable.psm1
+
+$sqlServerName = "SQL-PRD-C01"
+$sqlDataBaseName = "WindowsEventLogs"
+$sqlTableName = "UserLoginEvents"
 
 $allEvents = Get-WinEvent -LogName ForwardedEvents | Where-Object{$_.Id -ne 111} | Sort-Object TimeCreated
 
@@ -143,13 +70,11 @@ foreach ($curEvent in $allEvents){
 
         Default { }
     }
-    #Write-Output "Writing $($curObj.RecordID) to DataTable"
-    #Out-DataTable -InputObject $curObj
-    #Write-Output "DataTable Completed."
+
     $outEvents += $curObj
     
 }
 
 $outDataTable = Out-DataTable -InputObject $outEvents
 
-$outDataTable
+Write-DataTable -ServerInstance $sqlServerName -Database $sqlDataBaseName -TableName $sqlTableName -Data $outDataTable 
